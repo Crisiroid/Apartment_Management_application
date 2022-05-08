abb31f59b7caad80ad741623489da316f761458d
Crisiroid <csiroid@gmail.com>
2022-05-08 16:12:15 +0430

    register Form

--- src/main_login_page.java
+++ src/main_login_page.java
@@ -2,9 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class main_login_page {
-    private JPanel maing_login_panel;
+    JPanel maing_login_panel;
     private JLabel login_main_top_text;
     private JButton creditsButton;
     private JButton userLoginButton;
@@ -38,15 +42,5 @@ public class main_login_page {
                 //call Designed Admin Page here
             }
         });
-    }
-    public static void main(String[] args){
-        //Creating new Jframe
-        JFrame jf = new JFrame("Apartment Management Application");
-        //Setting Close operation
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jf.setContentPane(new main_login_page().maing_login_panel);
-        //Packing and showing main Frame
-        jf.pack();
-        jf.setVisible(true);
     }
 }
---
+++ src/registerLayout.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class registerLayout {
+    JPanel registerPanel;
+    private JLabel registerLabel;
+    private JTextField textField1;
+    private JTextField textField2;
+
+    public registerLayout(){
+        registerLabel.setFont(new Font("calibri", Font.BOLD, 23));
+    }
+}
