6a6848ddd4194923958e479c6735c2840267a2f2
Crisiroid <csiroid@gmail.com>
2022-05-08 19:41:20 +0430

    created database, register page, authentication and fixed bugs

--- src/mainLoader.java
+++ src/mainLoader.java
@@ -1,29 +1,25 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Scanner;
+
 
 public class mainLoader {
     public static void main(String[] args){
-        File autCheck = new File("aut.txt");
-        JFrame jf = new JFrame("Apartment Management Application Register");
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jf.setContentPane(new registerLayout().registerPanel);
-        jf.pack();
-        jf.setVisible(true);
-        if(autCheck.exists()){
-            //Creating new Jframe
-            jf = new JFrame("Apartment Management Application");
-            //Setting Close operation
-            jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            jf.setContentPane(new main_login_page().maing_login_panel);
-            //Packing and showing main Frame
-            jf.pack();
-            jf.setVisible(true);
-        }
+            File autCheck = new File("aut.txt");
+            JFrame jf = new JFrame("Apartment Management Application Register");
+            if(autCheck.exists()){
+                //Creating new Jframe
+                jf = new JFrame("Apartment Management Application");
+                //Setting Close operation
+                jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                jf.setContentPane(new main_login_page().maing_login_panel);
+                //Packing and showing main Frame
+                jf.pack();
+                jf.setVisible(true);
+            }else{
+                jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                jf.setContentPane(new registerLayout().registerPanel);
+                jf.pack();
+                jf.setVisible(true);
+            }
     }
 }
--- src/registerLayout.java
+++ src/registerLayout.java
@@ -1,13 +1,86 @@
 import javax.swing.*;
 import java.awt.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
 public class registerLayout {
+    JFrame jf;
+    //sqlite database connection variables
+    static Connection C = null;
+    static String sql = null;
     JPanel registerPanel;
+    static Statement stmt = null;
+    //swing related variables
     private JLabel registerLabel;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JLabel regAdminName;
+    private JLabel regPass;
+    private JLabel unitNum;
+    private JTextField regAdminField;
+    private JTextField regFloorNFrield;
+    private JTextField regUnitNField;
+    private JButton regSubBTN;
+    private JPasswordField regPasswordField;
 
-    public registerLayout(){
+    //Submitting inputs into database
+    public registerLayout() {
         registerLabel.setFont(new Font("calibri", Font.BOLD, 23));
+        connect();
+        regSubBTN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String adminName = regAdminField.getText();
+                int floorNumber = Integer.parseInt(regFloorNFrield.getText());
+                int unitesNumber = Integer.parseInt(regUnitNField.getText());
+                String passwordUnits = regPasswordField.getText();
+                System.out.println(adminName);
+                writeToDB(adminName, floorNumber, passwordUnits, unitesNumber);
+                try {
+                    File myObj = new File("aut.txt");
+                    if (myObj.createNewFile()) {
+                        System.out.println("File created: " + myObj.getName());
+                    } else {
+                        System.out.println("File already exists.");
+                    }
+                } catch (IOException ea) {
+                    System.out.println("An error occurred.");
+                    ea.printStackTrace();
+                }
+                mainLoader mainloader = new mainLoader();
+                String[] arguments = new String[] {"123"};
+                mainloader.main(arguments);
+                System.exit(1);
+                jf.dispose();
+            }
+        });
+    }
+    //opening connection using JDBC driver
+    public static void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            C = DriverManager.getConnection("jdbc:sqlite:database.db");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+    //Writing into Database
+    public static void writeToDB(String aName, int Fnum, String Passw, int uni){
+        try {
+            C.setAutoCommit(false);
+            stmt = C.createStatement();
+            sql = "INSERT INTO apartmentDetails (admin_name,floor_number,password,unit_per_floor) " +
+                    "VALUES ("+ aName + ", "+ Fnum +", "+Passw+", "+ uni +" );";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            C.commit();
+            C.close();
+        } catch (SQLException e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
     }
 }
